d3fd6796efaf2e17e84bf3a5a166b73554d2f7e4
PajeAutista <vitor_cunha@estudante.sc.senai.br>
2019-07-05 17:17:22 -0300

    separando o codigo para o tiago

--- pojeto jogo/jogoapp/js.js
+++ pojeto jogo/jogoapp/js.js
@@ -51,6 +51,8 @@ function imprimirArray(id, array) {
   }
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//codigo normal
 
 var listElement = document.querySelector("#app ul")
 var inputElement = document.querySelector("#app input")
